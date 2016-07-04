00b6815bfa3ba8c57ba67d93320d35209ff2dd47
Michael Cheng <mcheng.oc@gmail.com>
2016-07-04 14:59:55 -0700

    add users#edit, route, method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,28 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to splash_path
     else
-      redirect_to new_user
+      redirect_to new_user_path
     end
   end
 
   def show
     @user = User.find(params[:id])
     render :show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user[:id])
+    else
+      redirect_to new_user_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show", as: "user"
   get "/signup", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "/users/:id", to: "users#update"
 
 end
