38244b9a7183435bab774cc482572368963e44f8
Michael Cheng <mcheng.oc@gmail.com>
2016-07-03 20:41:36 -0700

    adds users#show page, route and method

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "/", to: "users#splash", as: "splash"
   get "/users", to: "users#index", as: "users"
+  get "/users/:id", to: "users#show", as: "user"
   get "/signup", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
 
