040509875b7be408e8c1822f3247e46213664683
Michael Cheng <mcheng.oc@gmail.com>
2016-07-18 10:50:15 -0700

    add profilepic capabilities with uploadcare

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user[:id])
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user[:id])
     else
       redirect_to new_user_path
     end
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:user_name, :first_name, :last_name, :email, :password, :password_confirmation, :image_url)
   end
 
 end
