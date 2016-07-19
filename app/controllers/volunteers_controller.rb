41b52d3c3ff760d2f1fa8822a41b6ba3a0e609c8
Michael Cheng <mcheng.oc@gmail.com>
2016-07-19 10:19:26 -0700

    add delete function

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -10,9 +10,18 @@ class VolunteersController < ApplicationController
     @bet = Bet.find(params[:bet_id])
     @volunteer = Volunteer.new({user_id: @user[:id], bet_id: @bet[:id]})
     if @volunteer.save
-      redirect_to bets_path
+      redirect_to bet_volunteers_path
     else
       redirect_to volunteers_path
+    end
+  end
+
+  def delete
+    @volunteer = Volunteer.find(params[:id])
+    if @volunteer.destroy
+      redirect_to bet_volunteers_path
+    else
+      redirect_to bet_volunteers_path
     end
   end
 
