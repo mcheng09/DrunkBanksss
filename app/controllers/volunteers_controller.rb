9b9f513e2f2dd5b0686ec86c3bd503445799c62c
Michael Cheng <mcheng.oc@gmail.com>
2016-07-19 17:34:33 -0700

    add logic to volunteer creation

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -9,20 +9,39 @@ class VolunteersController < ApplicationController
   def create
     @user = current_user
     @bet = Bet.find(params[:bet_id])
-    @volunteer = Volunteer.new({user_id: @user[:id], bet_id: @bet[:id]})
-    if @volunteer.save
-      redirect_to bet_volunteers_path
+    isvolunteer = false
+    @bet.volunteers do |volunteer|
+      if volunteer.user == current_user
+        isvolunteer = true
+        break
+      end
+    end
+    if isvolunteer = false
+      if current_user == @bet.user
+        flash[:error] = "You may not volunteer for a bet you created."
+        redirect_to splash_path
+      else
+        @volunteer = Volunteer.new({user_id: @user[:id], bet_id: @bet[:id]})
+        if @volunteer.save
+          redirect_to bet_volunteers_path
+        else
+          redirect_to volunteers_path
+        end
+      end
     else
-      redirect_to volunteers_path
+      flash[:error] = "You suck!"
+      redirect_to splash_path
     end
   end
 
   def delete
     @volunteer = Volunteer.find(params[:id])
+    @bet = Bet.find(params[:bet_id])
     if @volunteer.destroy
-      redirect_to bet_volunteers_path
+      flash[:error] = "Scaredy Pants."
+      redirect_to splash_path
     else
-      redirect_to bet_volunteers_path
+      redirect_to bet_path[@bet[:id]]
     end
   end
 
